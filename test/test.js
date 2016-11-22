1649d54b5efc387c3e2691c143d7c28be41285ac
Palmer, Matthew <Matthew.Palmer2@news.com.au>
2016-11-22 17:17:53 +1100

    Clean up

--- test/test.js
+++ test/test.js
@@ -3,15 +3,6 @@ var fs = require('fs');
 
 var input = fs.readFileSync(__dirname + '/simple.json').toString();
 
-// console.time('regular-parse');
-// JSON.parse(input);
-// console.timeEnd('regular-parse');
-
-// console.time('rapid-parse');
-// bindings.parse(input, input.length, (res) => {
-// 	console.timeEnd('rapid-parse');	
-// })
-
 console.time('regular-parse');
 for (var i = 0; i < 1000; i++) {
 	JSON.parse(input);
@@ -32,9 +23,3 @@ setInterval(() => {
 		process.exit()
 	}
 }, 20);
-
-
-// console.log('sending in', input.length);
-// bindings.parse(input, input.length, (result) => {
-// 	console.log('done');
-// })
